02d427b989c57bbb73de9cf4c438b3c9cf5371e6
TaroSunn <sunxiaomingyes@gmail.com>
2021-03-03 00:18:25 +0800

    feat: diff算法

--- src/index.js
+++ src/index.js
@@ -18,4 +18,34 @@ stateMixin(Vue)
 
 initGlobalApi(Vue)
 
+import {compileToFunction} from './compiler/index'
+import {createElm, patch} from './vdom/patch'
+
+let oldTemplate = `<div style="color:red;background: blue" a="1">
+  <li key="A">A</li> 
+  <li key="B">B</li>
+  <li key="C">C</li>
+  <li key="D">D</li>
+</div>`
+
+let vm1 = new Vue({data: {message: 'hello'}})
+const render1 = compileToFunction(oldTemplate)
+const oldVnode = render1.call(vm1)
+
+document.body.appendChild(createElm(oldVnode))
+
+let newTemplate = `<div b="1" style="color:green">
+  <li key="D">D</li>
+  <li key="A">A</li>
+  <li key="B">B</li>
+  <li key="C">C</li>
+</div>`
+let vm2 = new Vue({data: {message: 'test'}})
+const render2 = compileToFunction(newTemplate)
+const newVnode = render2.call(vm2)
+
+setTimeout(() => {
+  patch(oldVnode, newVnode)
+}, 2000);
+
 export default Vue
--- src/vdom/patch.js
+++ src/vdom/patch.js
@@ -11,6 +11,124 @@ export function patch(oldVnode, vnode) {
     parentElm.removeChild(oldVnode)
     
     return elm
+  } else {
+    // 比较标签名称不同
+    if(oldVnode.tag !== vnode.tag) {
+      return oldVnode.el.parentNode.replaceChild(createElm(vnode), oldVnode.el)
+    }
+    let el = vnode.el = oldVnode.el
+
+    // 比较文本
+    if(vnode.tag === undefined) { // 新旧都是文本
+      if(oldVnode.text !== vnode.text) {
+        el.textContent = vnode.text
+      }
+      return
+    }
+    // 比较属性
+    patchProps(vnode, oldVnode.data)
+    
+    let oldChildren = oldVnode.children || []
+    let newChildren = vnode.children || []
+
+    // 都有子元素
+    if(oldChildren.length > 0 && newChildren.length >0) {
+
+      pathChildren(el, oldChildren, newChildren)      
+
+    } else if(newChildren.length > 0) {
+      // 旧元素没有子元素，新元素有子元素
+      for(let i = 0; i < newChildren.length; i++) {
+        let child = createElm(newChildren[i])
+        el.appendChild(child)
+      }
+    } else if(oldChildren.length > 0) {
+      // 新元素没有子元素，旧元素有子元素
+      el.innerHTML = ''
+    }
+  }
+}
+
+function isSameVnode(oldVnode, newVnode) {
+  return (oldVnode.tag === newVnode.tag) && (oldVnode.key === newVnode.key)
+}
+
+function pathChildren(el, oldChildren, newChildren) {
+  let oldStartIndex = 0
+  let oldStartVnode = oldChildren[0]
+  let oldEndIndex = oldChildren.length - 1
+  let oldEndVnode = oldChildren[oldEndIndex]
+
+  let newStartIndex = 0
+  let newStartVnode = newChildren[0]
+  let newEndIndex = newChildren.length - 1
+  let newEndVnode = newChildren[newEndIndex]
+
+  while(oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) {
+    if(isSameVnode(oldStartVnode, newStartVnode)) {
+      patch(oldStartVnode, newStartVnode)
+      oldStartVnode = oldChildren[++oldStartIndex]
+      newStartVnode = newChildren[++newStartIndex]
+    }else if(isSameVnode(oldEndVnode, newEndVnode)) {
+      patch(oldEndVnode, newEndVnode)
+      oldEndVnode = oldChildren[--oldEndIndex]
+      newEndVnode = newChildren[--newEndIndex]
+    } else if (isSameVnode(oldStartVnode, newEndVnode)) {
+      patch(oldStartVnode , newEndVnode)
+      el.insertBefore(oldStartVnode.el, oldEndVnode.el.nextSibling)
+      oldStartVnode = oldChildren[++oldStartIndex]
+      newEndVnode = newChildren[--newEndIndex]
+    } else if(isSameVnode(oldEndVnode, newStartVnode)) {
+      patch(oldEndVnode, newStartVnode)
+      el.insertBefore(oldEndVnode.el, oldStartVnode.el)
+      oldEndVnode = oldChildren[--oldEndIndex]
+      newStartVnode = newChildren[++newStartIndex]
+    }
+  }
+
+  if(newStartIndex <= newEndIndex) {
+    for(let i = newStartIndex; i <= newEndIndex; i++) {
+      // el.appendChild(createElm(newChildren[i]))
+      let anchor = newChildren[newEndIndex + 1] == null ? null : newChildren[newEndIndex + 1].el
+      el.insertBefore(createElm(newChildren[i]), anchor)
+    }
+  }
+
+  if(oldStartIndex <= oldEndIndex) {
+    for(let i = oldStartIndex; i <= oldEndIndex; i++) {
+      el.removeChild(oldChildren[i].el)
+    }
+  }
+
+}
+
+function patchProps(vnode, oldProps = {}) {
+  let newProps = vnode.data || {}
+  let el = vnode.el
+
+  let newStyle = newProps.style || {}
+  let oldStyle = oldProps.style || {}
+
+  for (let key in oldStyle) {
+    if(!newStyle[key]) {
+      el.style[key] = ''
+    }
+  }
+
+  for(let key in oldProps) {
+    if(!newProps[key]) {
+      el.removeAttribute(key)
+    }
+  }
+
+  for(let key in newProps) {
+    if(key === 'style') {
+      for(let styleName in newProps.style) {
+        el.style[styleName] = newProps.style[styleName]
+      }
+    } else {
+      el.setAttribute(key, newProps[key])
+    }
   }
 }
 
@@ -24,7 +142,7 @@ function createComponent(vnode) {
   }
 }
 
-function createElm(vnode) {
+export function createElm(vnode) {
   let {tag, data, children, text, vm} = vnode
   if(typeof tag === 'string') {
 
@@ -33,6 +151,7 @@ function createElm(vnode) {
     }
 
     vnode.el = document.createElement(tag)
+    patchProps(vnode)
     children.forEach(child => {
       vnode.el.appendChild(createElm(child))
     })
